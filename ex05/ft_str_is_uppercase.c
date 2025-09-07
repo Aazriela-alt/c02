877d61864ba0db9729a9ca52ceff8753383e9f52
Noéllie SIRIGNANO <noellie@MacBook-Pro-de-lisa.local>
2025-09-07 16:10:00 +0200

    +

---
+++ ex05/ft_str_is_uppercase.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int	ft_str_is_uppercase(char *str)
+{
+	int a;
+	
+	a = 0;
+	while (str[a] != '\0')
+	{
+		if(! (str[a] >= 'A' && str[a] <= 'Z'))
+		{
+			return 0;
+		}
+		a++;
+	}
+	return 1;
+}
---
+++ ex08/ft_strlowcase.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int a;
+
+	a = 0;
+	while (str[a] != '\0')
+	{
+		if	(str[a] >= 'A' && str[a] <= 'Z')
+			str[a] = str[a] + 32;
+		a++;
+	}
+	return str;
+}
